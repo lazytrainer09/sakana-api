a1d2d4b629dfc2a15b922ea41b6b58f40cd2cc7f
dev-mori <kierurorentu@gmail.com>
2022-09-18 12:24:56 +0900

    Fix: for review

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :project do
     name { Faker::Lorem.sentence }
-
-    trait :too_long_name do
-      name { Faker::Lorem.characters(number: 101) }
-    end
   end
 end
